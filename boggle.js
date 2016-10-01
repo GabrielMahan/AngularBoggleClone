0fc5ffcb62da9f130a15185feb7d1a0613c3c8e7
Gabriel Mahan <mahan.gabe@gmail.com>
2016-09-30 21:22:12 -0400

    start word check method

--- boggle.js
+++ boggle.js
@@ -1,28 +1,28 @@
-var myApp = angular.module('myApp',[]);
-
-//myApp.directive('myDirective', function() {});
-//myApp.factory('myService', function() {});
-
-myApp.controller('Boggle', ['$scope', function($scope){
-	$scope.game = new BoggleBoard
-  $scope.board = $scope.game.randBoard();
-  $scope.reset = function() {
-  	var newBog = new BoggleBoard
-    $scope.board = newBog.randBoard();
-  }
-
-}])
+// var myApp = angular.module('myApp',[]);
+//
+// myApp.controller('Boggle', ['$scope', function($scope){
+// 	$scope.game = new BoggleBoard
+//   $scope.board = $scope.game.randBoard();
+//   $scope.reset = function() {
+//   	var newBog = new BoggleBoard
+//     $scope.board = newBog.randBoard();
+//   }
+//
+// }])
 
 
 var BoggleBoard = function() {
 	this.referenceCubes =  ["AAEEGN","ELRTTY","AOOTTW","ABBJOO","EHRTVW","CIMOTU","DISTTY","EIOSST","DELRVY","ACHOPS", "HIMNQU","EEINSU","EEGHNW","AFFKPS","HLNNRZ","DEILRX"];
-	this.letterCubeBag = this.referenceCubes;
 	this.cubeGrid = this.letterCubeBag;
+  this.letterCubeBag = this.referenceCubes;
 	this.board = [];
 };
 
+
+// board management helper methods
+
 BoggleBoard.prototype.clear = function() {
-	this.cubeGrid = [];
+	this.cubeGrid = this.referenceCubes;
 	this.board = []
 	this.letterCubeBag = this.referenceCubes;
 };
@@ -39,7 +39,6 @@ BoggleBoard.prototype.shake = function() {
 	for (var i = 0; i < 16; i++) {
     	this.pickCube();
 	}
-	console.log(this.cubeGrid)
 };
 
 BoggleBoard.prototype.fillBoardFromCubeGrid = function() {
@@ -51,12 +50,15 @@ BoggleBoard.prototype.fillBoardFromCubeGrid = function() {
 	}
 };
 
+// sets a random board
 BoggleBoard.prototype.randBoard = function() {
 	this.clear();
 	this.shake();
 	this.fillBoardFromCubeGrid();
 	return this.board;
 };
+
+// match checking methods
 BoggleBoard.prototype.horizontalMatch = function(i1, i2) {
 		if (Math.abs(i2-i1) <= 1 && Math.floor(i2 / 4) === Math.floor(i1 / 4) ) { return true }
 		else { return false }
@@ -76,3 +78,45 @@ BoggleBoard.prototype.nextTo = function(i1, i2) {
 	if (this.horizontalMatch(i1,i2) || this.verticalMatch(i1,i2) || this.diagonalMatch(i1,i2))  { return true }
 	else { return false }
 }
+
+BoggleBoard.prototype.getIndicies = function(letter) {
+  var indicies = [];
+  for (var i = 0; i < this.board.length; i++) {
+    if (letter.toUpperCase() === this.board[i]) {
+      indicies.push(i)
+    }
+  }
+  if (indicies.length === 0) { return false }
+  else { return indicies }
+}
+
+BoggleBoard.prototype.getWordRoutes = function(word) {
+  indexPaths = [this.getIndicies(word[0])];
+  if (!indexPaths) { return false };
+  for (var i = 1; i < word.length; i++ ) {
+    var letterIndicies = this.getIndicies(word[i])
+    if (letterIndicies) {
+      var replacedPath = []
+      while (letterIndicies.length > 0) {
+        var currentLetter = letterIndicies.pop()
+        for (var j = 0; j < indexPaths.length; j++) {
+          var path = indexPaths[j].push(currentLetter);
+          var lastTwo
+          if () {
+            replacedPath.push(path);
+          }
+        }
+      }
+      indexPaths = replacedPath
+    }
+    else {
+      return false
+    }
+  }
+  return indexPaths
+}
+
+// for each index in ....
+// check to see if there 's  any of the next ones work'
+
+// over each path, appends the index to the path.
